d146fede4b8677b13fd705d8d5e8f0e6d3c447cb
sonaligadekar20 <sonaliygadekar@gmail.com>
2023-12-07 11:00:25 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-center">Tic Tac Toe 🦉 🐧</h1>
+    <div className="board">
+    <div className='row'>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+      </div>
+      <div className='row'>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+      </div>
+      <div className='row'>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+      </div>
+
+    </div>
+
+    </div>
+  )
+}
+
+export default App
