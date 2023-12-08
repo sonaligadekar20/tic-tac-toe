aa80ee380c85773c354f44faffc303f137f598d1
sonaligadekar20 <sonaliygadekar@gmail.com>
2023-12-08 15:07:32 +0530

    players added

--- src/App.js
+++ src/App.js
@@ -1,29 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [player, setPlayer] = useState(2)
+  const [board, setBoard] = useState({
+    1: "",
+    2: "",
+    3: "",
+    4: "",
+    5: "",
+    6: "",
+    7: "",
+    8: "",
+    9: ""
+  })
+
+  const play = (boxNo) =>{
+     if(player===1){
+      setBoard({...board, [boxNo]: "🦉"})
+     }
+     else{
+      setBoard({...board, [boxNo]: "🐧"})
+     }
+    //  console.log(board)
+
+    setPlayer(player===1 ? 2 : 1)
+  }
   return (
     <div>
       <h1 className="text-center">Tic Tac Toe 🦉 🐧</h1>
+
+      <div className="players-container">
+        <span className="player-title">Player 1: 🦉 </span>
+
+        <span className="player-title">Player 2: 🐧 </span>
+      </div>
+      <p className="text-center current-player-text">
+        Current Player: {player === 1 ? "🦉": "🐧"}
+      </p>
     <div className="board">
     <div className='row'>
-        <div className="box"></div>
-        <div className="box"></div>
-        <div className="box"></div>
+        <div className="box" onClick={() =>{play(1)}}>
+          {board[1]}
+        </div>
+        <div className="box" onClick={() =>{play(2)}}>
+          {board[2]}
+        </div>
+        <div className="box" onClick={() =>{play(3)}}>
+          {board[3]}
+        </div>
       </div>
       <div className='row'>
-        <div className="box"></div>
-        <div className="box"></div>
-        <div className="box"></div>
+        <div className="box" onClick={() =>{play(4)}}>
+          {board[4]}
+        </div>
+        <div className="box" onClick={() =>{play(5)}}>
+          {board[5]}
+        </div>
+        <div className="box" onClick={() =>{play(6)}}>
+          {board[6]}
+        </div>
       </div>
       <div className='row'>
-        <div className="box"></div>
-        <div className="box"></div>
-        <div className="box"></div>
+        <div className="box" onClick={() =>{play(7)}}>
+        {board[7]}
+        </div>
+        <div className="box" onClick={() =>{play(8)}}>
+        {board[8]}
+        </div>
+        <div className="box" onClick={() =>{play(9)}}>
+        {board[9]}
+        </div>
       </div>
-
     </div>
-
     </div>
   )
 }
